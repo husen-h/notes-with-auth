d5e7cb5f2d4057840fe4c30a163e772d99dab30c
HUSEN <khamkhoev.khusen@gmail.com>
2023-08-20 19:54:29 +0300

    added notesRouter to all routes

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,6 +1,7 @@
 import { exampleRouter } from "~/server/api/routers/example";
 import { createTRPCRouter } from "~/server/api/trpc";
 import { registrationRouter } from "./routers/registration";
+import { notesRouter } from "./routers/notes";
 
 /**
  * This is the primary router for your server.
@@ -10,6 +11,7 @@ import { registrationRouter } from "./routers/registration";
 export const appRouter = createTRPCRouter({
   example: exampleRouter,
   registration: registrationRouter,
+  notes: notesRouter,
 });
 
 // export type definition of API
