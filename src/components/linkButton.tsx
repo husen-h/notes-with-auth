a073d39ac970ccba4d9ba2440044c69982cc3186
HUSEN <khamkhoev.khusen@gmail.com>
2023-08-20 19:54:48 +0300

    added onClick prop to LinkButton

--- src/components/linkButton.tsx
+++ src/components/linkButton.tsx
@@ -5,17 +5,19 @@ interface LinkProps {
   href: string;
   children?: ReactNode;
   type?: never;
+  onClick?: never;
 }
 
 interface ButtonProps {
   href?: never;
   children?: ReactNode;
   type?: ButtonHTMLAttributes<HTMLButtonElement>["type"];
+  onClick?: () => void;
 }
 
 type Props = LinkProps | ButtonProps;
 
-export function LinkButton({ href, children, type }: Props) {
+export function LinkButton({ href, children, type, onClick }: Props) {
   const styles =
     "border bg-slate-600 px-8 py-2 text-slate-100 transition hover:bg-slate-900 flex items-center gap-2";
 
