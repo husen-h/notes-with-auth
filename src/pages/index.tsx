1b51399852e599891d128de6feb00696507d11dd
HUSEN <khamkhoev.khusen@gmail.com>
2023-08-20 13:09:17 +0300

    added link to notes page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,46 +1,48 @@
-import { type GetServerSideProps } from "next";
-import { getServerSession } from "next-auth";
-import { signOut, useSession } from "next-auth/react";
-import { authOptions } from "~/server/auth";
+import { useSession } from "next-auth/react";
+import Link from "next/link";
+import { HiArrowLongRight } from "react-icons/hi2";
+import { LayoutWithTitle } from "~/components/layoutWithTitle";
 
 export default function Home() {
-  const { data: sessionData, status } = useSession();
+  const { status } = useSession();
 
   return (
-    <>
-      <h1 className="text-8xl font-bold">
-        <span>Gradient </span>
-        <span className="bg-gradient-to-r from-blue-700 to-rose-600 bg-clip-text text-transparent">
-          Text
-        </span>
-      </h1>
-      {status === "authenticated" && (
-        <button
-          className="border border-slate-900 px-4 py-2 hover:bg-slate-600 hover:text-slate-100"
-          onClick={() => signOut()}
+    <LayoutWithTitle title={"Notes-taking"}>
+      {status === "unauthenticated" ? (
+        <Link
+          className="border bg-slate-600 px-8 py-2 text-slate-100 transition hover:bg-slate-900"
+          href="/auth/register"
         >
-          Sign out
-        </button>
+          Sign in
+        </Link>
+      ) : (
+        <Link
+          href="/notes"
+          className="group flex items-center border border-l-0 border-r-0 border-t-0 border-b-blue-600 text-blue-600"
+        >
+          <p className="pr-2 text-xl transition-[padding] duration-200 ease-in group-hover:pr-6">
+            Continue to notes
+          </p>
+          <HiArrowLongRight size={28} className="text-blue-600" />
+        </Link>
       )}
-    </>
+    </LayoutWithTitle>
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const session = await getServerSession(context.req, context.res, authOptions);
-  console.log(session);
-  if (!session) {
-    return {
-      redirect: {
-        destination: "auth/register/",
-        permanent: false,
-      },
-    };
-  }
+// export const getServerSideProps: GetServerSideProps = async (context) => {
+//   const session = await getServerSession(context.req, context.res, authOptions);
+//   console.log(session);
+//   if (!session) {
+//     return {
+//       redirect: {
+//         destination: "auth/register/",
+//         permanent: false,
+//       },
+//     };
+//   }
 
-  return {
-    props: {},
-  };
-};
-
-type AuthorizationTabType = "login" | "register";
+//   return {
+//     props: {},
+//   };
+// };
