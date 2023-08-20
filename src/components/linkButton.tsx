fdce5c3f23ea111ff02d6f5dfa449f53debe4ca3
HUSEN <khamkhoev.khusen@gmail.com>
2023-08-20 13:08:41 +0300

    added linkButton component

---
+++ src/components/linkButton.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import { type ButtonHTMLAttributes, type ReactNode } from "react";
+
+interface LinkProps {
+  href: string;
+  children?: ReactNode;
+  type?: never;
+}
+
+interface ButtonProps {
+  href?: never;
+  children?: ReactNode;
+  type?: ButtonHTMLAttributes<HTMLButtonElement>["type"];
+}
+
+type Props = LinkProps | ButtonProps;
+
+export function LinkButton({ href, children, type }: Props) {
+  const styles =
+    "border bg-slate-600 px-8 py-2 text-slate-100 transition hover:bg-slate-900 flex items-center gap-2";
+
+  if (href) {
+    return (
+      <Link href={href} className={styles}>
+        {children}
+      </Link>
+    );
+  }
+
+  return (
+    <button type={type} className={styles}>
+      {children}
+    </button>
+  );
+}
