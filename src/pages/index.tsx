e91dbeec4911cdfaa43d5faeb98f92f7770e5d5a
HUSEN <khamkhoev.khusen@gmail.com>
2023-08-21 12:42:39 +0300

    removed useSession hook from home-page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
-import { GetServerSideProps } from "next";
+import {
+  type InferGetServerSidePropsType,
+  type GetServerSideProps,
+} from "next";
 import { getServerSession } from "next-auth";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
@@ -6,20 +9,12 @@ import { HiArrowLongRight } from "react-icons/hi2";
 import { LayoutWithTitle } from "~/components/layoutWithTitle";
 import { authOptions } from "~/server/auth";
 
-export default function Home() {
-  const { status } = useSession();
-
+export default function Home({
+  loggedIn,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <LayoutWithTitle title={"Notes-taking"}>
-      {status === "unauthenticated" && (
-        <Link
-          className="border bg-slate-600 px-8 py-2 text-slate-100 transition hover:bg-slate-900"
-          href="/auth/register"
-        >
-          Sign in
-        </Link>
-      )}
-      {status === "authenticated" && (
+      {loggedIn ? (
         <Link
           href="/notes"
           className="group flex items-center border border-l-0 border-r-0 border-t-0 border-b-blue-600 text-blue-600"
@@ -29,8 +24,25 @@ export default function Home() {
           </p>
           <HiArrowLongRight size={28} className="text-blue-600" />
         </Link>
+      ) : (
+        <Link
+          className="border bg-slate-600 px-8 py-2 text-slate-100 transition hover:bg-slate-900"
+          href="/auth/register"
+        >
+          Sign in
+        </Link>
       )}
-      {status === "loading" && <div>Checking authentication...</div>}
     </LayoutWithTitle>
   );
 }
+
+export const getServerSideProps: GetServerSideProps<{
+  loggedIn: boolean;
+}> = async (context) => {
+  const session = await getServerSession(context.req, context.res, authOptions);
+  return {
+    props: {
+      loggedIn: Boolean(session),
+    },
+  };
+};
